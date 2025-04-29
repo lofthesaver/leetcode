3c569dd7faa3534950e6234e4d2196dd7917f370
lofthesaver <chang_yichen@u.nus.edu>
2025-04-29 08:42:14 +0800

    sliding window template

--- Algorithm Templates/sliding_window.py
+++ Algorithm Templates/sliding_window.py
@@ -3,17 +3,24 @@
 # Pseudocode framework for sliding window algorithm
 def slidingWindow(s: str):
 
-    # Use an appropriate data structure to record the data
-    # in the window, which can vary depending on the problem
+    # Window datastructure
     window = ...
 
     # Initial window
-    left, right = 0, 0
+    left = 0
 
-    while right < len(s):
+    # Record result
+    res = 0
+
+    # Loop through right index
+    for right in range(len(s)):
+
+        # Add right element to window
+        right_elem = s[right]
+        window.add(right_elem)
 
         # Determine whether the left side of the window needs to be contracted
-        while left < right and window needs shrink:
+        while left <= right and window needs shrink:
 
             # Remove left element from window
             left_elem = s[left]
@@ -22,21 +29,7 @@ def slidingWindow(s: str):
             # Shrink the window
             left += 1
             
-            # Perform a series of updates on the data within the window
-            ...
-
-        # Add right element to window
-        right_elem = s[right]
-        window.add(right_elem)
-
-        # Expand the window
-        right += 1
-
-        # Perform a series of updates on the data within the window
-        ...
-
-        # *** position for debug output ***
-        # Note that you should not print in the final solution code
-        # because IO operations are time-consuming and may cause timeouts
-        # print(f"window: [{left}, {right})")
-        # ***********************
+        # Record result, left is the first index where condition is not satisfied
+        res += left
+    
+    return res
