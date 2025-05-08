c4d76e3785048486ca04d998bdb723bdec4ec000
lofthesaver <chang_yichen@u.nus.edu>
2025-05-08 10:18:08 +0800

    may 07 2025

--- 3341_find_minimum_time_to_reach_last_room_i.py
+++ 3341_find_minimum_time_to_reach_last_room_i.py
@@ -8,23 +8,19 @@ class Solution:
         num_rows = len(moveTime)
         num_cols = len(moveTime[0])
 
-        # Record visited
-        visited = [[0 for _ in range(num_cols)] for _ in range(num_rows)]
-
         # Record min costs
         min_costs = [[float('inf') for _ in range(num_cols)] for _ in range(num_rows)]
         min_costs[0][0] = 0
 
-        # Initialize pq, start with initial point
-        pq = [(0, 0)]
+        # Initialize pq, start with time and initial point
+        pq = [(0, 0, 0)]
         heapq.heapify(pq)
 
         # While pq is not empty
         while pq:
 
             # Pop current point and set to visited
-            curr_row, curr_col = heapq.heappop(pq)
-            visited[curr_row][curr_col] = 1
+            curr_cost, curr_row, curr_col = heapq.heappop(pq)
 
             # Explore neighbours
             for neighbor_row, neighbor_col in [(curr_row + 1, curr_col), (curr_row - 1, curr_col), (curr_row, curr_col + 1), (curr_row, curr_col - 1)]:
@@ -40,7 +36,7 @@ class Solution:
                         
                         # Update min_costs
                         min_costs[neighbor_row][neighbor_col] = neighbor_cost
-                        heapq.heappush(pq, (neighbor_row, neighbor_col))
+                        heapq.heappush(pq, (neighbor_cost, neighbor_row, neighbor_col))
 
         # Return cost it takes to reach room (n - 1, m - 1)
         return min_costs[-1][-1]
